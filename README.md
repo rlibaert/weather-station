027a1cb1fb7749d48d1ba3ba7e5a7a5cfd5da7ea
Romain Libaert <git@rlibaert.dev>
2026-06-26 21:40:22 +0200

    feat: read from BME280 :tada:

--- README.md
+++ README.md
@@ -1,11 +1,53 @@
 # Weather Station
 
-Firmware code for a weather station that uses an ESP32-C3 microcontroller to
-collect and display weather data.
+> [!IMPORTANT]
+> This is a toy project for me to learn Rust and embedded development and it's still under heavy development.
+
+Firmware code for a weather station based on an ESP32-C3 microcontroller and BME280 sensor.
 
 ## Getting Started
 
+### Connection diagram
+
+```mermaid
+flowchart LR
+
+subgraph ESP32[Seeed Studio ESP32-C3]
+  esp_sda[I2C_SDA]
+  esp_scl[I2C_SCL]
+  esp_3v3[3V3]
+  esp_gnd[GND]
+end
+
+subgraph GY-BME280
+  bme_vcc[VCC]
+  bme_gnd[GND]
+  bme_scl[SCL]
+  bme_sda[SDA]
+  bme_vcc --- CSB
+  bme_gnd --- SDO
+end
+
+esp_sda --- bme_sda
+esp_scl --- bme_scl
+esp_3v3 --- bme_vcc
+esp_gnd --- bme_gnd
+```
+
+### Run the code
+
 ```bash
 $ cargo run --release
-Bing!
+initializing I2C...
+initializing BME280...
+starting loop...
+trying to read from BME280...
+T: 32.658386 °C
+H: 47.545048 %
+P: 101351.7 Pa
+
+trying to read from BME280...
+T: 32.65711 °C
+H: 47.45209 %
+P: 101351.7 Pa
 ```
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,9 +7,10 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
+use bme280::i2c::BME280;
 use esp_hal::{
     clock::CpuClock,
-    main,
+    i2c, main,
     time::{Duration, Instant},
 };
 
@@ -38,10 +39,32 @@ fn main() -> ! {
 
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 66320);
 
+    esp_println::println!("initializing I2C...");
+
+    let i2c = i2c::master::I2c::new(_peripherals.I2C0, i2c::master::Config::default())
+        .unwrap()
+        .with_sda(_peripherals.GPIO6)
+        .with_scl(_peripherals.GPIO7);
+
+    const BME280_I2C_ADDR: u8 = 0x76; // SDO connected to GND
+    let mut bme280 = BME280::new(i2c, BME280_I2C_ADDR);
+    let mut delay = esp_hal::delay::Delay::new();
+
+    esp_println::println!("initializing BME280...");
+    bme280.init(&mut delay).unwrap();
+
+    esp_println::println!("starting loop...");
     loop {
-        esp_println::println!("Bing!");
+        esp_println::println!("trying to read from BME280...");
+
+        let measurements = bme280.measure(&mut delay).unwrap();
+        esp_println::println!("T: {} °C", measurements.temperature);
+        esp_println::println!("H: {} %", measurements.humidity);
+        esp_println::println!("P: {} Pa", measurements.pressure);
+        esp_println::println!("");
+
         let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(1000) {}
+        while delay_start.elapsed() < Duration::from_millis(2000) {}
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.1.0/examples
