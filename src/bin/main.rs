a3efcdc39173cd2ea282f2a2ade5edc2fc4e2c71
Romain Libaert <git@rlibaert.dev>
2026-06-28 18:01:52 +0200

    refacto: init & task of bme

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -38,47 +38,61 @@ async fn main(spawner: embassy_executor::Spawner) {
     // generator parameters: --chip esp32c3 -o unstable-hal -o alloc -o ci -o zed
 
     esp_println::logger::init_logger(log::LevelFilter::Debug);
+    info!("Starting");
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
 
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 66320);
 
-    let timg0 = TimerGroup::new(_peripherals.TIMG0);
-    let sw_int = SoftwareInterruptControl::new(_peripherals.SW_INTERRUPT);
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let sw_int = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
     esp_rtos::start(timg0.timer0, sw_int.software_interrupt0);
 
     let i2c = {
-        info!("initializing I2C...");
+        debug!("Initializing I2C...");
         let config = i2c::master::Config::default();
-        i2c::master::I2c::new(_peripherals.I2C0, config)
+        i2c::master::I2c::new(peripherals.I2C0, config)
             .unwrap()
-            .with_sda(_peripherals.GPIO6)
-            .with_scl(_peripherals.GPIO7)
+            .with_sda(peripherals.GPIO6)
+            .with_scl(peripherals.GPIO7)
     };
 
+    let bme280 = {
+        debug!("Initializing BME280...");
+        const ADDRESS: u8 = 0x76; // SDO connected to GND
+        let mut bme280 = BME280::new(i2c, ADDRESS);
+        bme280
+            .init(&mut embassy_time::Delay)
+            .inspect_err(|_| error!("Failed to initialize BME280"))
+            .unwrap();
+        bme280
+    };
+    spawner.spawn(task_bme280(bme280).unwrap());
+
     let spi = {
-        info!("initializing SPI...");
+        debug!("Initializing SPI...");
         let config = spi::master::Config::default()
-            .with_frequency(Rate::from_khz(100))
+            .with_frequency(Rate::from_mhz(1))
             .with_mode(spi::Mode::_0);
-        spi::master::Spi::new(_peripherals.SPI2, config)
+        spi::master::Spi::new(peripherals.SPI2, config)
             .inspect_err(|e| error!("Error while creating SPI: {e}"))
             .unwrap()
-            .with_sck(_peripherals.GPIO8)
-            .with_mosi(_peripherals.GPIO10)
+            .with_sck(peripherals.GPIO8)
+            .with_mosi(peripherals.GPIO10)
     };
 
     let mut driver = {
+        debug!("Initializing EPD...");
         use esp_hal::gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull};
 
         let busy = Input::new(
-            _peripherals.GPIO2,
+            peripherals.GPIO2,
             InputConfig::default().with_pull(Pull::Up),
         );
-        let res = Output::new(_peripherals.GPIO3, Level::High, OutputConfig::default());
-        let dc = Output::new(_peripherals.GPIO4, Level::Low, OutputConfig::default());
-        let cs = Output::new(_peripherals.GPIO5, Level::High, OutputConfig::default());
+        let res = Output::new(peripherals.GPIO3, Level::High, OutputConfig::default());
+        let dc = Output::new(peripherals.GPIO4, Level::Low, OutputConfig::default());
+        let cs = Output::new(peripherals.GPIO5, Level::High, OutputConfig::default());
 
         let device = embedded_hal_bus::spi::ExclusiveDevice::new(spi, cs, embassy_time::Delay)
             .inspect_err(|e| error!("Error creating exclusive spi device: {e}"))
@@ -90,12 +104,13 @@ async fn main(spawner: embassy_executor::Spawner) {
     driver.init().unwrap();
 
     let mut display = weact_studio_epd::graphics::Display213BlackWhite::new();
-    display.set_rotation(weact_studio_epd::graphics::DisplayRotation::Rotate90);
+    display.set_rotation(weact_studio_epd::graphics::DisplayRotation::Rotate270);
 
     let style = embedded_graphics::mono_font::MonoTextStyle::new(
         &profont::PROFONT_24_POINT,
         weact_studio_epd::Color::Black,
     );
+
     let _ = embedded_graphics::text::Text::with_text_style(
         "Hello World!",
         embedded_graphics::geometry::Point::new(8, 68),
@@ -105,27 +120,13 @@ async fn main(spawner: embassy_executor::Spawner) {
     .draw(&mut display);
 
     driver.full_update(&display).unwrap();
-
-    spawner.spawn(task_bme280(i2c).unwrap());
-    spawner.spawn(task_dummy().unwrap());
+    driver.sleep().unwrap();
 }
 
 #[embassy_executor::task]
-async fn task_bme280(i2c: i2c::master::I2c<'static, esp_hal::Blocking>) {
-    let mut delay = embassy_time::Delay;
-    let mut bme280 = {
-        info!("initializing BME280...");
-        const ADDRESS: u8 = 0x76; // SDO connected to GND
-        let mut bme280 = BME280::new(i2c, ADDRESS);
-        bme280.init(&mut delay).unwrap();
-        bme280
-    };
-
-    info!("starting loop...");
+async fn task_bme280(mut bme280: BME280<i2c::master::I2c<'static, esp_hal::Blocking>>) {
     loop {
-        debug!("reading sensor...");
-
-        let measurements = bme280.measure(&mut delay).unwrap();
+        let measurements = bme280.measure(&mut embassy_time::Delay).unwrap();
         println!(
             "[{:.1} °C] [{:.1} %] [{:.1} hPa]",
             measurements.temperature,
@@ -133,14 +134,6 @@ async fn task_bme280(i2c: i2c::master::I2c<'static, esp_hal::Blocking>) {
             measurements.pressure / 100.0
         );
 
-        embassy_time::Timer::after_millis(5_000).await;
-    }
-}
-
-#[embassy_executor::task]
-async fn task_dummy() {
-    loop {
-        info!("Hello world from embassy!");
-        embassy_time::Timer::after_millis(10_000).await;
+        embassy_time::Timer::after_secs(60).await;
     }
 }
